7484cdf6e657ad2989e0daa81963b35f98165c08
buggy <buggy778@qq.com>
2015-06-09 21:32:14 +0800

    1

--- tmp/setup_god8.sh
+++ tmp/setup_god8.sh
@@ -3,8 +3,9 @@
 # Website: http://zhsunlight.cn, http://zhflash.com
 #
 
-odoo_url = https://github.com/buggy778/odoo-cn/archive/8.0.zip
-odoo_runtime = https://github.com/buke/GreenOdoo-Runtime/archive/linux64.zip
+odoo_url=https://github.com/buggy778/odoo-cn/archive/8.0.zip
+odoo_runtime_url=https://github.com/buke/GreenOdoo-Runtime/archive/linux64.zip
+ssd_url=http://zhflash.com/wp-content/download/start-stop-daemon.zip
 
 sudo rm -rf /etc/init.d/od8
 sudo rm -rf /etc/init.d/pg8
@@ -21,7 +22,7 @@ sudo useradd openerp
 sudo apt-get install wget unzip gcc
 cd /tmp
 sudo wget $odoo_url -O odoo.zip
-wget $odoo_runtime -O runtime.zip
+wget $odoo_runtime_url -O runtime.zip
 unzip odoo.zip
 sudo mv odoo /home/od8
 rm -fr odoo.zip
@@ -31,7 +32,7 @@ rm -fr runtime.zip
 sudo mkdir /home/od8/log
 sudo chown -R openerp:openerp /home/od8
 
-wget http://zhflash.com/wp-content/download/start-stop-daemon.zip
+wget $ssd_url
 unzip start-stop-daemon.zip
 cd start-stop-daemon
 sudo gcc start-stop-daemon.c -o start-stop-daemon
