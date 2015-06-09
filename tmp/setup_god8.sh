a1589122826da76a1981423bdfe99555cd697af2
buggy <buggy778@qq.com>
2015-06-09 21:16:54 +0800

    1

--- tmp/setup_god8.sh
+++ tmp/setup_god8.sh
@@ -6,36 +6,36 @@
 odoo_url = https://github.com/buggy778/odoo-cn/archive/8.0.zip
 odoo_runtime = https://github.com/buke/GreenOdoo-Runtime/archive/linux64.zip
 
-rm -rf /etc/init.d/od8
-rm -rf /etc/init.d/pg8
-cp od8 /etc/init.d
-cp pg8 /etc/init.d
-chmod 755 /etc/init.d/od8
-chmod 755 /etc/init.d/pg8
-chown root: /etc/init.d/od8
-chown root: /etc/init.d/pg8
-chkconfig --levels 235 od8 on
-chkconfig --levels 235 pg8 on
+sudo rm -rf /etc/init.d/od8
+sudo rm -rf /etc/init.d/pg8
+sudo cp od8 /etc/init.d
+sudo cp pg8 /etc/init.d
+sudo chmod 755 /etc/init.d/od8
+sudo chmod 755 /etc/init.d/pg8
+sudo chown root: /etc/init.d/od8
+sudo chown root: /etc/init.d/pg8
+sudo chkconfig --levels 235 od8 on
+sudo chkconfig --levels 235 pg8 on
 
-useradd openerp
+sudo useradd openerp
 sudo apt-get install wget unzip gcc
 cd /tmp
-wget $odoo_url -O odoo.zip
+sudo wget $odoo_url -O odoo.zip
 wget $odoo_runtime -O runtime.zip
 unzip odoo.zip
-mv odoo /home/od8
+sudo mv odoo /home/od8
 rm -fr odoo.zip
 unzip runtime.zip
-mv runtime /home/od8
+sudo mv runtime /home/od8
 rm -fr runtime.zip
-mkdir /home/od8/log
-chown -R openerp:openerp /home/od8
+sudo mkdir /home/od8/log
+sudo chown -R openerp:openerp /home/od8
 
 wget http://zhflash.com/wp-content/download/start-stop-daemon.zip
 unzip start-stop-daemon.zip
 cd start-stop-daemon
-gcc start-stop-daemon.c -o start-stop-daemon
-cp start-stop-daemon /usr/sbin/
+sudo gcc start-stop-daemon.c -o start-stop-daemon
+sudo cp start-stop-daemon /usr/sbin/
 cd ..
 rm -rf start-stop-daemon
 rm -rf start-stop-daemon.zip
